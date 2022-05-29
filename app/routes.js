ab989b3504b2a5bc13b866a33ac70c5798e09ed4
Alexandra Harley <aharleydev@gmail.com>
2022-05-28 21:28:56 -0400

    connected to db, needs profile styling to track dog stats, light styling

---
+++ app/routes.js
@@ -0,0 +1,72 @@
+module.exports = function (app, passport, db) {
+    // home page
+    app.get('/', function (req, res) {
+        res.render('index.ejs')
+    })
+
+    // profile section
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user,
+        })
+    })
+
+    app.get('/logout', function (req, res) {
+        req.logout()
+        res.redirect('/')
+    })
+
+    // CRUD methods will go here.
+
+    // =============================================================================
+    // AUTHENTICATE (FIRST LOGIN) ==================================================
+    // =============================================================================
+
+    // locally --------------------------------
+    // LOGIN ===============================
+    // show the login form
+    app.get('/login', function (req, res) {
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+    // SIGNUP =================================
+    app.get('/signup', function (req, res) {
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
+
+    // =============================================================================
+    // UNLINK ACCOUNTS =============================================================
+    // =============================================================================
+
+    // local -----------------------------------
+    app.get('/unlink/local', isLoggedIn, function (req, res) {
+        var user = req.user;
+        user.local.email = undefined;
+        user.local.password = undefined;
+        user.save(function (err) {
+            res.redirect('/profile');
+        });
+    });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
